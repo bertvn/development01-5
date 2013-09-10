468f885cac1dcb5ca6c522a9914985aaaf13eb86
Bert van Nimwegen <bert_v_n@hotmail.com>
2013-09-10 09:52:21 +0200

    first commit

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: Bert
+ *
+ * Created on September 10, 2013, 9:36 AM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
